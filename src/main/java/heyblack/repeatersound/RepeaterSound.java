2c3e3986aa978d94643841819b13e44929c212bb
HeyBlack <zhfleo@163.com>
2024-12-08 18:36:12 +0800

    v1.5.0 update

--- src/main/java/heyblack/repeatersound/RepeaterSound.java
+++ src/main/java/heyblack/repeatersound/RepeaterSound.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import heyblack.repeatersound.config.ConfigManager;
+import heyblack.repeatersound.util.InteractionMode;
 import heyblack.repeatersound.util.ServerCloseCallback;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -13,6 +14,8 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RepeaterSound implements ClientModInitializer
 {
@@ -23,6 +26,8 @@ public class RepeaterSound implements ClientModInitializer
     public static final SoundEvent BLOCK_REDSTONE_WIRE_CLICK = register("repeatersound:redstone_wire_click");
     public static final SoundEvent BLOCK_DAYLIGHT_DETECTOR_CLICK = register("repeatersound:daylight_detector_click");
     public static final SoundEvent CLICK_ALARM = register("repeatersound:click_alarm");
+
+    public static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void onInitializeClient()
@@ -60,9 +65,10 @@ public class RepeaterSound implements ClientModInitializer
                                 .suggests(
                                         (ctx, builder) ->
                                         {
-                                            builder.suggest("NORMAL");
-                                            builder.suggest("ALARM");
-                                            builder.suggest("DISABLED");
+                                            for (InteractionMode type : InteractionMode.values())
+                                            {
+                                                builder.suggest(type.id);
+                                            }
 
                                             return builder.buildFuture();
                                         }
