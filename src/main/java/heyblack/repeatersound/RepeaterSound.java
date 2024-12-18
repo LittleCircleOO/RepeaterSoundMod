35d379c4188ca8f6441649d8734ee6162de7020f
HeyBlack <zhfleo@163.com>
2024-12-18 18:28:19 +0800

    v1.6.0 update

--- src/main/java/heyblack/repeatersound/RepeaterSound.java
+++ src/main/java/heyblack/repeatersound/RepeaterSound.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import heyblack.repeatersound.config.ConfigManager;
+import heyblack.repeatersound.config.ConfigOption;
 import heyblack.repeatersound.util.InteractionMode;
 import heyblack.repeatersound.util.ServerCloseCallback;
 import net.fabricmc.api.ClientModInitializer;
@@ -27,7 +28,7 @@ public class RepeaterSound implements ClientModInitializer
     public static final SoundEvent BLOCK_DAYLIGHT_DETECTOR_CLICK = register("repeatersound:daylight_detector_click");
     public static final SoundEvent CLICK_ALARM = register("repeatersound:click_alarm");
 
-    public static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void onInitializeClient()
@@ -37,69 +38,81 @@ public class RepeaterSound implements ClientModInitializer
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, environment) -> dispatcher.register(
                 ClientCommandManager.literal("repeatersound")
                         .then(ClientCommandManager.literal("setBasePitch")
-                                .then(ClientCommandManager.argument("basePitch", FloatArgumentType.floatArg())
-                                .executes(ctx -> cfg.setConfig(
-                                        "basePitch",
-                                        String.valueOf(FloatArgumentType.getFloat(ctx, "basePitch")),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                                .then(ClientCommandManager.argument(ConfigOption.BASE_PITCH.id, FloatArgumentType.floatArg())
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.BASE_PITCH.id,
+                                                String.valueOf(FloatArgumentType.getFloat(ctx, ConfigOption.BASE_PITCH.id)),
+                                                ctx.getSource().getPlayer()
+                                        ))))
 
-                .then(ClientCommandManager.literal("useRandomPitch")
-                        .then(ClientCommandManager.argument("useRandom", BoolArgumentType.bool())
-                                .executes(ctx -> cfg.setConfig(
-                                        "useRandom",
-                                        String.valueOf(BoolArgumentType.getBool(ctx, "useRandom")),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                        .then(ClientCommandManager.literal("useRandomPitch")
+                                .then(ClientCommandManager.argument(ConfigOption.USE_RANDOM.id, BoolArgumentType.bool())
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.USE_RANDOM.id,
+                                                String.valueOf(BoolArgumentType.getBool(ctx, ConfigOption.USE_RANDOM.id)),
+                                                ctx.getSource().getPlayer()
+                                        ))))
 
-                .then(ClientCommandManager.literal("setVolume")
-                        .then(ClientCommandManager.argument("volume", FloatArgumentType.floatArg())
-                                .executes(ctx -> cfg.setConfig(
-                                        "volume",
-                                        String.valueOf(FloatArgumentType.getFloat(ctx, "volume")),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                        .then(ClientCommandManager.literal("setVolume")
+                                .then(ClientCommandManager.argument(ConfigOption.VOLUME.id, FloatArgumentType.floatArg())
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.VOLUME.id,
+                                                String.valueOf(FloatArgumentType.getFloat(ctx, ConfigOption.VOLUME.id)),
+                                                ctx.getSource().getPlayer()
+                                        ))))
 
-                .then(ClientCommandManager.literal("interactionMode")
-                        .then(ClientCommandManager.argument("mode", StringArgumentType.string())
-                                .suggests(
-                                        (ctx, builder) ->
-                                        {
-                                            for (InteractionMode type : InteractionMode.values())
-                                            {
-                                                builder.suggest(type.id);
-                                            }
+                        .then(ClientCommandManager.literal("interactionMode")
+                                .then(ClientCommandManager.argument(ConfigOption.INTERACTION_MODE.id, StringArgumentType.string())
+                                        .suggests(
+                                                (ctx, builder) ->
+                                                {
+                                                    for (InteractionMode type : InteractionMode.values())
+                                                    {
+                                                        builder.suggest(type.id);
+                                                    }
 
-                                            return builder.buildFuture();
-                                        }
-                                )
-                                .executes(ctx -> cfg.setConfig(
-                                        "interactionMode",
-                                        StringArgumentType.getString(ctx, "mode"),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                                                    return builder.buildFuture();
+                                                }
+                                        )
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.INTERACTION_MODE.id,
+                                                StringArgumentType.getString(ctx, ConfigOption.INTERACTION_MODE.id),
+                                                ctx.getSource().getPlayer()
+                                        ))))
 
-                .then(ClientCommandManager.literal("alarmMessage")
-                        .then(ClientCommandManager.argument("message", StringArgumentType.string())
-                                .executes(ctx -> cfg.setConfig(
-                                        "alarmMessage",
-                                        String.valueOf(StringArgumentType.getString(ctx, "message")),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                        .then(ClientCommandManager.literal("alarmMessage")
+                                .then(ClientCommandManager.argument(ConfigOption.INTERACTION_MODE.id, StringArgumentType.string())
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.INTERACTION_MODE.id,
+                                                String.valueOf(StringArgumentType.getString(ctx, ConfigOption.INTERACTION_MODE.id)),
+                                                ctx.getSource().getPlayer()
+                                        ))))
 
-                .then(ClientCommandManager.literal("disabledMessage")
-                        .then(ClientCommandManager.argument("message", StringArgumentType.string())
-                                .executes(ctx -> cfg.setConfig(
-                                        "disabledMessage",
-                                        String.valueOf(StringArgumentType.getString(ctx, "message")),
-                                        ctx.getSource().getPlayer()
-                                ))))
+                        .then(ClientCommandManager.literal("disabledMessage")
+                                .then(ClientCommandManager.argument(ConfigOption.DISABLED_MESSAGE.id, StringArgumentType.string())
+                                        .executes(ctx -> cfg.setConfigCommand(
+                                                ConfigOption.DISABLED_MESSAGE.id,
+                                                String.valueOf(StringArgumentType.getString(ctx, ConfigOption.DISABLED_MESSAGE.id)),
+                                                ctx.getSource().getPlayer()
+                                        ))))
         ));
 
         ServerCloseCallback.EVENT.register(cfg);
     }
 
     private static SoundEvent register(String id) {
-        return (SoundEvent)Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(new Identifier(id)));
+        return (SoundEvent) Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(new Identifier(id)));
+    }
+
+    public static void info(String s) {
+        LOGGER.info("[RepeaterSound] " + s);
+    }
+
+    public static void warn(String s) {
+        LOGGER.warn("[RepeaterSound] " + s);
+    }
+
+    public static void error(String s) {
+        LOGGER.error("[RepeaterSound] " + s);
     }
 }
