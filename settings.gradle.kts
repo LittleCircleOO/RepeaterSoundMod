9334fa5897641d376ab1e4bbbbd1ead37276b80f
LittleCircleOO <51949761+LittleCircleOO@users.noreply.github.com>
2025-10-24 16:37:50 +0800

    StoneCutter Init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,33 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+
+        //StoneCutter
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.7.10"
+}
+
+stonecutter {
+    create(rootProject) {
+        versions(
+            "1.16.5",
+            "1.17.1",
+            "1.18.2",
+            "1.19.2",
+            "1.20.1",
+            "1.20.4",
+            "1.20.6",
+            "1.21.1",
+            "1.21.3",
+            "1.21.4",
+            "1.21.5",
+            "1.21.8",
+            "1.21.10"
+        )
+        vcsVersion = "1.21.10"
     }
 }
